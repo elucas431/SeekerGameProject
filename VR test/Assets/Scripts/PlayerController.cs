86526e74e74d82d17e1677bddfd1c1c74494835a
elucas431 <elucas2@asu.edu>
2018-02-05 23:04:46 -0700

    Basic Setup
    First Iteration. Basic VR movement, Two cameras in scene, trap spheres trigger on space bar press.

---
+++ VR test/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    [SerializeField]
+    private Transform rig;
+    private Valve.VR.EVRButtonId touchpad = Valve.VR.EVRButtonId.k_EButton_SteamVR_Touchpad;
+    public float speed; 
+
+    private SteamVR_TrackedObject trackedObj;
+    private SteamVR_Controller.Device controller { get { return SteamVR_Controller.Input((int)trackedObj.index); } }
+
+    private Vector2 axis = Vector2.zero;
+
+    void Start () {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+	}
+	
+	void Update () { 
+        if (controller == null)
+        {
+            Debug.Log("Controller not initialized");
+            return;
+        }
+
+        var device = SteamVR_Controller.Input((int)trackedObj.index);
+        
+        if (controller.GetTouch(touchpad))
+        {
+            axis = device.GetAxis(Valve.VR.EVRButtonId.k_EButton_Axis0);
+
+            if (rig != null)
+            {
+                rig.position += (transform.right * axis.x + transform.forward * axis.y) * Time.deltaTime * speed;
+                rig.position = new Vector3(rig.position.x, 0, rig.position.z);
+            }
+        }
+    }
+}
---
+++ VR test/Assets/Scripts/TrapControl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapControl : MonoBehaviour {
+
+    float moveTrap = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space) && moveTrap < 11.0f)
+        {
+            moveTrap += 0.2f;
+        }
+
+        else if (moveTrap >= 11.0f)
+        {
+            moveTrap = 10.0f;
+        }
+        this.transform.Translate(0.0f, 0.0f, moveTrap);
+
+       
+	}
+}
