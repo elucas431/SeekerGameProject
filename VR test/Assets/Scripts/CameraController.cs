2f0cb94fd7de3f34d0c85725d266b9653fd03113
elucas431 <elucas2@asu.edu>
2018-02-13 14:33:11 -0700

    Test Test Test

--- VR test/Assets/Scripts/CameraController.cs
+++ VR test/Assets/Scripts/CameraController.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-
     public float panSpeed = 20f;
     public float panBoarderThickness = 10f;
     public Vector2 panLimit;
@@ -37,7 +36,7 @@ public class CameraController : MonoBehaviour
 
         float scroll = Input.GetAxis("Mouse ScrollWheel");
         position.y -= scroll * scrollSpeed * 100f * Time.deltaTime;
-        /*
+        /* testing branch
         position.x = Mathf.Clamp(position.x, -panLimit.x, panLimit.x);
         position.y = Mathf.Clamp(position.y, minY, maxY);
         position.z = Mathf.Clamp(position.z, -panLimit.y, panLimit.y);
