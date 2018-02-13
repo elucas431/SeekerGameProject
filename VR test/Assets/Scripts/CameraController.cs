513e18692e6ac8bb3f6aeff30ae26c7401a0719d
FullmetalDBZ <agavini1@asu.edu>
2018-02-13 14:27:31 -0700

    Testing branch

--- VR test/Assets/Scripts/CameraController.cs
+++ VR test/Assets/Scripts/CameraController.cs
@@ -37,7 +37,7 @@ public class CameraController : MonoBehaviour
 
         float scroll = Input.GetAxis("Mouse ScrollWheel");
         position.y -= scroll * scrollSpeed * 100f * Time.deltaTime;
-        /*
+        /* testing branch
         position.x = Mathf.Clamp(position.x, -panLimit.x, panLimit.x);
         position.y = Mathf.Clamp(position.y, minY, maxY);
         position.z = Mathf.Clamp(position.z, -panLimit.y, panLimit.y);
