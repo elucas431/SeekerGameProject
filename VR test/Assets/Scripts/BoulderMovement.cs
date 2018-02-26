df90533c1297c043a2a182284ffc676ff6298fbf
FullmetalDBZ <36206879+FullmetalDBZ@users.noreply.github.com>
2018-02-26 12:57:16 -0700

    Temp Boulder

---
+++ VR test/Assets/Scripts/BoulderMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoulderMovement : MonoBehaviour {
+
+    public Rigidbody rb;
+    public bool active = false;
+
+	// Use this for initialization
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) { 
+		//if (active == true)
+        //{
+            rb.useGravity = true;
+        }
+	}
+}
