35e31a3d2a7188f40945ed7a86c5e25a9c4b1c46
FullmetalDBZ <agavini1@asu.edu>
2018-02-27 14:25:41 -0700

    Boulder with Spacebar

--- VR test/Assets/Scripts/BoulderMovement.cs
+++ VR test/Assets/Scripts/BoulderMovement.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class BoulderMovement : MonoBehaviour {
 
     public Rigidbody rb;
+    //public GameObject trigger;
     public bool active = false;
 
 	// Use this for initialization
 	void Start ()
     {
         rb = GetComponent<Rigidbody>();
+        //active = trigger.GetComponent<ActivateBoulder>().activate;
 	}
 	
 	// Update is called once per frame
