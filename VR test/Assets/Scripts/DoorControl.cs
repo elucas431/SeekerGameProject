3d414c9e8a65a6f4a9367388615d95206bcb80ec
elucas431 <elucas2@asu.edu>
2018-04-02 15:40:08 -0700

    FinishedLevelLayout/DoorControl

---
+++ VR test/Assets/Scripts/DoorControl.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+public class DoorControl : MonoBehaviour
+{
+    [SerializeField] private int LoadLevel;
+    public int relicCount;
+    public GameObject relic; 
+
+    private void Start()
+    {
+        relicCount = 0; 
+    }
+
+    private void Update()
+    {
+        
+    }
+
+
+    void OnCollisionEnter(Collision other)
+
+    {
+        if (relicCount >= 3)
+        {
+
+            if (other.gameObject.CompareTag("Player"))
+            {
+                SceneManager.LoadScene(sceneBuildIndex: LoadLevel);
+
+            }
+        }
+    }
+}
