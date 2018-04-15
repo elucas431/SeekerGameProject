d231a0ac7c42b0eedbdd755972aa93645f1e549c
FullmetalDBZ <agavini1@asu.edu>
2018-04-15 09:11:00 -0700

    Arrow Trap Temp

---
+++ VR test/Assets/Scripts/ArrowSpawner.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowSpawner : MonoBehaviour {
+
+    public GameObject arrowPrefab;
+    public float startTime, spawnDelay;
+
+	// Use this for initialization
+	void Start ()
+    {
+        InvokeRepeating("SpawnObject", startTime, spawnDelay);
+	}
+	
+	// Update is called once per frame
+	public void SpawnObject ()
+    {
+        Instantiate(arrowPrefab, transform.position, transform.rotation);
+	}
+}
